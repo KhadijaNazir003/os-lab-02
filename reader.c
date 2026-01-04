3e433fdba9f437b206f0c59facd57ec6f061553c
KhadijaNazir003 <149264854+KhadijaNazir003@users.noreply.github.com>
2026-01-04 20:52:49 +0500

    Refactor to add prime number filtering with pipes
    Implement a filter function for reading and processing prime numbers using pipes.

--- reader.c
+++ reader.c
@@ -1,13 +1,53 @@
-// Named Pipe (FIFO) Reader
 #include <stdio.h>
-#include <fcntl.h>
 #include <unistd.h>
+#include <stdlib.h>
+#include <sys/wait.h>
 
-int main() {
-    int fd = open("myfifo", O_RDONLY);
-    char buf[50];
-    read(fd, buf, sizeof(buf));
-    printf("Reader got: %s\n", buf);
-    close(fd);
-    return 0;
+/*
+ * Each filter:
+ * 1. Reads first number -> prime
+ * 2. Prints prime
+ * 3. Creates a new pipe
+ * 4. Forks next filter
+ * 5. Sends non-multiples to next filter
+ */
+
+void filter(int read_fd) {
+    int prime;
+
+    // Read first number (prime)
+    if (read(read_fd, &prime, sizeof(int)) <= 0) {
+        close(read_fd);
+        exit(0);
+    }
+
+    printf("%d ", prime);
+    fflush(stdout);
+
+    int fd[2];
+    pipe(fd);
+
+    pid_t pid = fork();
+
+    if (pid == 0) {
+        // Child: next filter
+        close(fd[1]);
+        close(read_fd);
+        filter(fd[0]);
+    } else {
+        // Parent: filter numbers
+        close(fd[0]);
+
+        int num;
+        while (read(read_fd, &num, sizeof(int)) > 0) {
+            if (num % prime != 0) {
+                write(fd[1], &num, sizeof(int));
+            }
+        }
+
+        close(read_fd);
+        close(fd[1]);
+        wait(NULL);
+        exit(0);
+    }
 }

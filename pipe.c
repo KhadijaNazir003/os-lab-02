6eccb5f6b7247d6480277122127e94a16ac2eb54
KhadijaNazir003 <149264854+KhadijaNazir003@users.noreply.github.com>
2026-01-04 20:54:00 +0500

    Refactor pipe.c to use filter function and write integers
    Refactor pipe.c to implement a filter function and handle integer writing from parent to child process.

--- pipe.c
+++ pipe.c
@@ -1,21 +1,33 @@
-// Anonymous Pipes
 #include <stdio.h>
 #include <unistd.h>
-#include <string.h>
+#include <stdlib.h>
+#include <sys/wait.h>
+
+// Declaration of filter function from reader.c
+void filter(int read_fd);
 
 int main() {
     int fd[2];
-    char buf[50];
     pipe(fd);
 
-    if (fork() == 0) { // Child
-        close(fd[0]);
-        char msg[] = "Hello from child";
-        write(fd[1], msg, strlen(msg)+1);
-    } else { // Parent
+    pid_t pid = fork();
+
+    if (pid == 0) {
+        // Child: start first filter
         close(fd[1]);
-        read(fd[0], buf, sizeof(buf));
-        printf("Parent received: %s\n", buf);
+        filter(fd[0]);
+    } else {
+        // Parent: generate numbers 2 to 1000
+        close(fd[0]);
+
+        for (int i = 2; i <= 1000; i++) {
+            write(fd[1], &i, sizeof(int));
+        }
+
+        close(fd[1]);
+        wait(NULL);
     }
+
     return 0;
 }
+
